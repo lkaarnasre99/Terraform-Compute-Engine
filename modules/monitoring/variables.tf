8898c4014ac8c5745ca8c76a1f55a3dd678cb398
lkaarnasre99 <165937094+lkaarnasre99@users.noreply.github.com>
2025-02-19 00:48:52 -0500

    errors

--- modules/monitoring/variables.tf
+++ modules/monitoring/variables.tf
@@ -28,19 +28,3 @@ variable "notification_channels" {
 }
 
 
-## Module: monitoring/outputs.tf
-
-output "cpu_alert_policy_id" {
-  description = "The ID of the CPU alert policy"
-  value       = google_monitoring_alert_policy.cpu_usage.id
-}
-
-output "memory_alert_policy_id" {
-  description = "The ID of the memory alert policy"
-  value       = google_monitoring_alert_policy.memory_usage.id
-}
-
-output "dashboard_id" {
-  description = "The ID of the VM dashboard"
-  value       = google_monitoring_dashboard.vm_dashboard.id
-}
