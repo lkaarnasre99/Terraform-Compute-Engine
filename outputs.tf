83c1a0abc91d40d8c10da2a01d2b90cdc65f1d21
lkaarnasre99 <165937094+lkaarnasre99@users.noreply.github.com>
2025-02-22 23:20:29 -0500

    build

--- outputs.tf
+++ outputs.tf
@@ -19,32 +19,3 @@ output "vm_self_link" {
   value       = module.lamp_vm.self_link
 }
 
-output "cpu_alert_policy_id" {
-  description = "The ID of the CPU alert policy"
-  value       = var.enable_monitoring ? module.monitoring[0].cpu_alert_policy_id : null
-}
-
-output "memory_alert_policy_id" {
-  description = "The ID of the memory alert policy"
-  value       = var.enable_monitoring ? module.monitoring[0].memory_alert_policy_id : null
-}
-
-output "dashboard_id" {
-  description = "The ID of the VM dashboard"
-  value       = var.enable_monitoring ? module.monitoring[0].dashboard_id : null
-}
-
-output "uptime_check_id" {
-  description = "The ID of the uptime check"
-  value       = var.enable_monitoring ? module.monitoring[0].uptime_check_id : null
-}
-
-output "uptime_alert_policy_id" {
-  description = "The ID of the uptime alert policy"
-  value       = var.enable_monitoring ? module.monitoring[0].uptime_alert_policy_id : null
-}
-
-output "network_alert_policy_id" {
-  description = "The ID of the network traffic alert policy"
-  value       = var.enable_monitoring ? module.monitoring[0].network_alert_policy_id : null
-}
