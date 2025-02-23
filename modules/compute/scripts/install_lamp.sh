151c86528cd9edec6ed91b493504f9a3fbfa89cd
lkaarnasre99 <165937094+lkaarnasre99@users.noreply.github.com>
2025-02-22 23:45:05 -0500

    agent install

--- modules/compute/scripts/install_lamp.sh
+++ modules/compute/scripts/install_lamp.sh
@@ -2,6 +2,12 @@
 #!/bin/bash
 set -e
 
+# Install Google Cloud Ops Agent
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+sudo systemctl status google-cloud-ops-agent"*"
+
+
 # Update package repository
 sudo apt-get update
 
