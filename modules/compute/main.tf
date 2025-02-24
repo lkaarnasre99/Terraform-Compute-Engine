ed625e774a4422a8dc17690e17da1f08acab8fd7
lkaarnasre99 <165937094+lkaarnasre99@users.noreply.github.com>
2025-02-24 00:49:26 -0500

    Update main.tf

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0.0"  # adjust version as needed
+    }
+  }
+}
+
+
+
+
 resource "google_compute_instance" "vm_instance" {
   name         = var.instance_name
   machine_type = var.machine_type
